c8a4b7abcacb7bf01aa5badfc7cb8e444a94bced
mdsayeed6220 <sayeedofficial6220@gmail.com>
2020-09-17 20:07:56 +0530

    Added Better Images displaying better pokecards

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./pokecard.css";
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// const POKE_API =
+//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+const padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 class Pokecard extends React.Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
